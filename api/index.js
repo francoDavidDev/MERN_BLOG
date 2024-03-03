18a9c17ea2fb3e22c14eb0389a2d81fd7647b8a6
francodavid_dev <francodavid_dev@outlook.com>
2024-03-03 03:52:26 -0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,23 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv' //para configurar variable entorno
+
+ dotenv.config();
+
+//conectamos con la base de datos
+mongoose
+  .connect(
+    process.env.MONGODB
+  )
+  .then(() => {
+    console.log("MongoDb Is connected");
+  }).catch((err)=>{
+    console.log('Hubo un error',err)
+  })
 
 //creamos una api con express
-
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!!')
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!!");
+});
